5ebbf92065c663fe77fe1acb88b2f8e027a4640c
Anna Horodetska and Max Reichman <max@>
2017-03-23 09:06:38 -0700

    Add branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-  
+  $("#animalform").submit(function(event){
+    var animalInput = $("#animal").val();
+    if (animalInput === "Lion") {
+      $("#lion").show();
+      $("#tiger").hide();
+      $("#fox").hide();
+    }
+    else if (animalInput === "Tiger") {
+      $("#tiger").show();
+      $("#lion").hide();
+      $("#fox").hide();
 
+    }
+    else {
+      $("#fox").show();
+      $("#lion").hide();
+      $("#tiger").hide();
+    }
+    event.preventDefault();
+  });
 });
